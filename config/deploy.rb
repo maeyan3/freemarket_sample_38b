d15cda541f58318e90da3cf34d7ffe9d6634d047
Kazushi Yamanishi <keironia187149@gmail.com>
2018-11-07 09:51:23 +0900

    capistranoの設定 unicornの設定 今後必要になるS3へのアクセスキーを環境変数とする記述を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,45 @@
 lock "~> 3.11.0"
 
-set :application, "freemarket_sample_38b"
-set :repo_url, "git@github.com:kazyam1001/freemarket_sample_38b"
+# 環境変数を使うよう明示
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
-set :deploy_to, '/var/www/freemarket_sample_38b'
-set :scm, :git
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_files, %w{ config/secrets.yml }
+set :application, "freemarket_sample_38b"
+set :repo_url, "git@github.com:KazYam1001/freemarket_sample_38b.git"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
+
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/kazyam/.ssh/mercari187.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-set :log_level, :debug
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-
-  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  # ローカル環境のsecrets.ymlを本番環境に反映するための記述
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
