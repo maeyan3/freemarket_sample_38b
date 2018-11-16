358d965aa351c7f045caf6567392800ef2ef878c
T:MASTER <ssstechz@TMASTERnoMacBook-puro.local>
2018-11-16 18:00:58 +0900

    add：新規機能追加
    # what
    商品一覧表示追加
    
    # why
    必須機能のため

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 describe ItemsController, type: :controller do
 
+  describe 'GET #index' do
+    it "populates an array of item ordered by created_at DESC" do
+    end
+
+    it "renders the :index template" do
+    end
+  end
+
   describe 'GET #new' do
 
     let(:user) { create(:user) }
