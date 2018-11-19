40ab66f8bcd5b3ce9980514c8cd6c0f4fcf7eb57
T:MASTER <ssstechz@TMASTERnoMacBook-puro.local>
2018-11-19 14:02:23 +0900

    add：1.新規 商品詳細ページ機能追加
    # what
    新規 商品詳細ページ機能追加
    
    # Why
    必須機能のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
   get 'users/new', to: 'users#new'
   get '/logout' => 'users#logout'
 end
