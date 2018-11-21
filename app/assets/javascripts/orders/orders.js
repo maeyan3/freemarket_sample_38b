2e188cb7e1a33ba3efeee8abbe110ddc6af9fc7f
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-11-21 21:57:10 +0900

    Newページの使用ポイント入力欄のjs

--- app/assets/javascripts/orders/orders.js
+++ app/assets/javascripts/orders/orders.js
@@ -3,4 +3,23 @@ $(function () {
     $(this).next("ul").slideToggle(400);
     $(this).toggleClass("open");
   });
+
+
+
+  $('.input-default_point').click(function() {
+    $('input[name=consume_point_radio]').val(['partial']);
+  });
+
+  $('.input-default_point').on('keyup', function(){
+    var input_point = $('.input-default_point').val();
+    var total_price = Math.floor(499 - input_point );
+    if (!isNaN(input_point)){
+    $('.total_price').html(total_price);
+    }else{
+      alert("半角数字で入力してください");
+    }
+  });
+
+
+
 });
