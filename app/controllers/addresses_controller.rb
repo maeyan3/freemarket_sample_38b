1fbc64ed2beeb943dfe87c059bd9539045b1e3ba
Manami0623 <manami.eikaiwa1b@gmail.com>
2018-11-22 13:53:28 +0900

    本人確認
    住所、本人情報、DB保存

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,10 +1,21 @@
 class AddressesController < ApplicationController
+
+
+  def index
+     @address = Address.all
+
+  end
+
   def new
     @prefectures = Prefecture.all
+    @address = Address.new
+    @address_id = Address.find(current_user.id)
   end
 
   def create
-
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to new_address_path
   end
 
   def edit
@@ -20,4 +31,11 @@ class AddressesController < ApplicationController
   def check_user
     redirect_to root_path if params[:user_id] != current_user.id
   end
+
+  def address_params
+    # binding.pry
+    params.require(:address).permit(:first_name, :last_name, :first_name_reading, :last_name_reading, :postal_code, :city,:block, :prefecture_id).merge(user_id: current_user.id)
+  end
+
+
 end
--- app/controllers/userconfirms_controller.rb
+++ app/controllers/userconfirms_controller.rb
@@ -1,15 +1,31 @@
 class UserconfirmsController < ApplicationController
-  def new
+
+  before_action :move_to_index, except: :index
+
+  def index
+    @user_details = UserDetail.all
 
   end
 
+  def new
+    @users = UserDetail.new
+    @address = Address.find(params[:address_id])
+  end
+
   def create
-     Users.create(user_params[:name],birthday:user_params[:birthday], address: tweet_params[:address])
+    @address = Address.find(params[:address_id])
+    @users = UserDetail.new(users_params)
+    @users.save
+    redirect_to new_address_userconfirm_path(@address)
 
   end
 
  private
     def users_params
-      params.permit(:name, :birthday, :address)
+      params.require(:user_detail).permit(:birth_day, :birth_month, :birth_year).merge(user_id: current_user.id,address_id: @address.id)
+    end
+
+    def move_to_index
+      redirect_to action: :index unless current_user
     end
 end
