683001eebdae779cf8676aac73f6ad3dec691042
Kazushi Yamanishi <keironia187149@gmail.com>
2018-11-22 12:15:39 +0900

    Prefixを使ってないないリンクを使うように修正 ルーティングの記述をusersにネストさせ簡略化

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only: [:index, :new, :create, :show]
-  get '/:user_id/logout' => 'users#logout'
-  get '/:user_id/mypage' => 'users#mypage'
-  get '/users/:user_id/profile' => 'profiles#edit'
-  patch '/users/:user_id/profile' => 'profiles#update'
   resources :credits, only: [:index, :new, :create, :destroy]
   resources :orders,  only: [:new, :create]
   resources :users, only: [:new] do
     resources :userconfirms, only: [:new, :create]
     resources :addresses, only: [:new, :create, :edit, :update]
+    get 'logout' => 'users#logout'
+    get 'mypage' => 'users#mypage'
+    get 'profile' => 'profiles#edit'
+    patch 'profile' => 'profiles#update'
   end
 end
