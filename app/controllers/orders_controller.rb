5bae2b7f210fcb22868a706c19c746b69755818c
T:MASTER <ssstechz@TMASTERnoMacBook-puro.local>
2018-11-23 19:42:05 +0900

    fix: pay.jp public_key
    # what
    テストキー削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,11 @@ class OrdersController < ApplicationController
   def pay
     Payjp.api_key = PAYJP_SECRET_KEY
     charge = Payjp::Charge.create(
-    :amount => 2100,
+    :amount => 3380,
     :card => params['payjp-token'],
     :currency => 'jpy',
   )
+     flash[:notice] = '購入が完了しました。'
   end
 
   def new
