9729cd2ab3b0477a9dd8f7993bef8ddac7951910
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-11-29 20:01:01 +0900

    Showページの調節

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,9 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @items = Item.all.includes(:item_images).page(params[:page]).per(20).order("created_at DESC")
+    @category = Category.find(params[:id])
+    @items = Category.find(params[:id]).items.order("created_at DESC").page(params[:page]).per(20)
   end
+
+
 end
