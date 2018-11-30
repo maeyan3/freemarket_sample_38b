7fa615e2a8d82a45e49295a062d5e14fea8b51ff
Kazushi Yamanishi <keironia187149@gmail.com>
2018-11-30 15:48:30 +0900

    Add sales moneys view

--- db/schema.rb
+++ db/schema.rb
@@ -148,7 +148,7 @@ ActiveRecord::Schema.define(version: 20181129092143) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "nickname", null: false
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
